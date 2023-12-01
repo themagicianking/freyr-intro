4ae73718ee2b463f0b5ca97174a244cf76003b18
themagicianking <tmoonder@gmail.com>
2023-12-01 17:12:57 -0500

    Restyled layout.

--- js/index.js
+++ js/index.js
@@ -9,39 +9,45 @@ copyright.innerHTML = "© Thane Chaves " + thisYear;
 footer.appendChild(copyright);
 
 //Web development profciencies and soft skills
-proficiencies = ["HTML/CSS", "Javascript", "Git", "Ruby", "Python"]
+skills = ["HTML/CSS", "Javascript", "Git", "Ruby", "Python"]
 softSkills = ["strong writing and reading comprehension", "animal handling", "dog grooming", "swimming and swim instruction", "receptionist skills", "interpersonal skills", "childcare and development including specializing in children with special needs", "basic first aid training"];
 
-proficienciesSection = document.getElementById("proficiencies");
-proficienciesList = proficienciesSection.querySelector("ul");
-addItemsToList(proficienciesList, proficiencies);
+skillsSection = document.getElementById("skills-section");
+skillsList = skillsSection.querySelector("ul");
+addItemsToList(skillsList, "skills", skills);
 
-function addItemsToList(listElement, list) {
+function addItemsToList(listElement, listClass, list) {
   for (let i = 0; i < list.length; i++) {
     let item = document.createElement("li");
+    item.setAttribute("class", listClass)
     item.innerHTML = list[i];
     listElement.appendChild(item);
   };
 };
 
-softSkillsSection = document.getElementById("soft-skills");
+softSkillsSection = document.getElementById("soft-skills-section");
 softSkillsList = softSkillsSection.querySelector("ul");
-addItemsToList(softSkillsList, softSkills);
+addItemsToList(softSkillsList, "soft-skills", softSkills);
 
-experience = [{company: "Sit Means Sit", location: "Richmond, VA", position: "Dog Trainer", timeline: "September 2023 - Present"}, {company: "Whole Foods", location: "Richmond, VA", position: "Team Member", timeline: "May 2023 - September 2023"}, {company: "Dog Krazy", location: "Fredericksburg & Richmond, VA", position: "Warehouse Worker, Retail Associate, Dog Groomer", timeline: "January 2022 - April 2023"}, {company: "UPS", location: "Fredericksburg, VA", position: "Seasonal Parcel Carrier", timeline: "December 2021"}, {company: "Silverbrook Kennels", location: "Fredericksburg, VA", position: "Kennel Attendant", timeline: "March 2021 - November 2021"}, {company: "Kid's First Swim School", location: "Fredericksburg, VA", position: "Swim Instructor & Receptionist", timeline: "June 2019 - April 2020"}, {company: "Dogtopia", location: "Fredericksburg, VA", position: "Kennel Attendant", timeline: "October 2019 - March 2020"}, {company: "Starbucks", location: "Fredericksburg, VA", position: "Barista", timeline: "August 2018 - December 2018"}];
+experience = [{company: "company", location: "location", position: "position", timeline: "timeline"}, {company: "Domino's Pizza", location: "Richmond, VA", position: "Delivery Driver", timeline: "August 2023 - Present"}, {company: "Sit Means Sit", location: "Richmond, VA", position: "Dog Trainer", timeline: "September 2023 - Present"}, {company: "Whole Foods", location: "Richmond, VA", position: "Team Member", timeline: "May 2023 - September 2023"}, {company: "Dog Krazy", location: "Fredericksburg & Richmond, VA", position: "Warehouse Worker, Retail Associate, Dog Groomer", timeline: "January 2022 - April 2023"}, {company: "UPS", location: "Fredericksburg, VA", position: "Seasonal Parcel Carrier", timeline: "December 2021"}, {company: "Silverbrook Kennels", location: "Fredericksburg, VA", position: "Kennel Attendant", timeline: "March 2021 - November 2021"}, {company: "Kid's First Swim School", location: "Fredericksburg, VA", position: "Swim Instructor & Receptionist", timeline: "June 2019 - April 2020"}, {company: "Dogtopia", location: "Fredericksburg, VA", position: "Kennel Attendant", timeline: "October 2019 - March 2020"}, {company: "Starbucks", location: "Fredericksburg, VA", position: "Barista", timeline: "August 2018 - December 2018"}];
 
 //Creates a list item for each past job
-experienceSection = document.getElementById("experience");
+experienceSection = document.getElementById("experience-section");
 experienceList = experienceSection.querySelector("ul");
 experience.forEach((job) => {
   newExperience = document.createElement("li");
+  newExperience.setAttribute("class", "experience")
   let company = document.createElement("span");
+  company.setAttribute("class", "experience")
   company.innerHTML = job.company;
   let location = document.createElement("span");
+  location.setAttribute("class", "experience")
   location.innerHTML = job.location;
   let position = document.createElement("span");
+  position.setAttribute("class", "experience")
   position.innerHTML = job.position;
   let timeline = document.createElement("span");
+  timeline.setAttribute("class", "experience")
   timeline.innerHTML = job.timeline;
   newExperience.appendChild(company);
   newExperience.appendChild(location);
@@ -60,11 +66,11 @@ messageForm.addEventListener("submit", function () {
   message = event.target.usersMessage.value;
 
   //Checks for existence of submitted messages before adding the message header and correct background color and border if a message is added
-  messageSection = document.getElementById("messages");
+  messageSection = document.getElementById("messages-section");
   if (messageSection.querySelector("ul").innerHTML.trim() == "") {
     messageSection.style.backgroundColor = "#463834";
     messageSection.style.border = "thick #81726A solid";
-    messageHeader = document.createElement("h1");
+    messageHeader = document.createElement("h2");
     messageHeader.innerHTML = "Messages";
     messageSection.prepend(messageHeader);
   };
@@ -141,13 +147,16 @@ fetch("https://api.github.com/users/themagicianking/repos?per_page=100")
   .then(data => {
     repositories = data;
     console.log(repositories);
-    projectSection = document.getElementById("projects");
+    projectSection = document.getElementById("projects-section");
     projectList = projectSection.querySelector("ul");
     repositories.filter((repo) => portfolioProjects.includes(repo.name)).forEach((repo) => {
       project = document.createElement("li");
+      project.setAttribute("class", "projects")
       projectDescription = document.createElement("span");
+      projectDescription.setAttribute("class", "projects")
       projectDescription.innerText = " — " + repo.description;
       projectLink = document.createElement("a");
+      projectLink.setAttribute("class", "projects")
       projectLink.innerText = repo.name;
       projectLink.href = repo.html_url;
       project.appendChild(projectLink);
